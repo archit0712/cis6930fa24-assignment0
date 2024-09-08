d7b53c8e5f0ddb7f253721c12eec18775ae653ce
Archit Mittal <e19cse041@gmail.com>
2024-09-08 17:57:35 -0400

    First Test added

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,6 +1,6 @@
 import main
 
 def test_download():
-    data = main.main(page=52)
-    assert data is None, 'Failed to download data from API'
+    data = main.fetch_data_from_api(page=1)
+    assert data is not None, 'Failed to download data from API'
     assert len(data['items']) > 0, 'No items on the page'
